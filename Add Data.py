012c3344612929693eaddbf8d40f230f2d3f874f
uwaiskgn <uwaiskgn@gmail.com>
2023-08-01 13:42:13 +0530

    Modified code

--- Add Data.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-todayDate = datetime.now()
-current_year = todayDate.strftime('%Y')
-current_time = todayDate.strftime("%H:%M:%S")
-print("Current time:", current_time)
-if current_year > '2023':
-    print("Current Year True:")
-else:
-    print("Current  Year false:")
