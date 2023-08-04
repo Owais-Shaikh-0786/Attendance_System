de59cba8c69d73d2f2c97bfd7e09b2642d50cece
uwaiskgn <uwaiskgn@gmail.com>
2023-08-04 19:10:26 +0530

    firebase update (latest code )

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Importing the mode images into a list
+# Import mode images into a list
 folderModePath = 'Resources'
 modePathList = os.listdir(folderModePath)
 imgModeList = []
@@ -18,6 +18,7 @@ for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 print(len(imgModeList))
 
+# Load the encoded face data
 print("Loading Encode File ...")
 with open('encodeFile.p', 'rb') as file:
     encodeListKnownWithIds = pickle.load(file)
@@ -31,7 +32,7 @@ firebase_admin.initialize_app(cred, {
 })
 db = firestore.client()
 
-
+# Function to get attendance status based on time
 def get_attendance_status(class_start_time, attendance_cutoff_time):
     now = datetime.now().time()
 
@@ -40,7 +41,7 @@ def get_attendance_status(class_start_time, attendance_cutoff_time):
     elif now >= attendance_cutoff_time:
         return "Late"
 
-
+# Function to get class start time and attendance cutoff time from user
 def get_class_time_input():
     class_start_time_str = input("Enter the class start time (format: HH:MM): ")
     attendance_cutoff_time_str = input("Enter the attendance cutoff time (format: HH:MM): ")
@@ -50,48 +51,61 @@ def get_class_time_input():
 
     return class_start_time, attendance_cutoff_time
 
+# Function to retrieve student details from Firestore
+def retrieve_student_details():
+    student_details = []
 
-async def markattendanceasync(stu_id, class_start_time, attendance_cutoff_time):
-    # Get the current date and time
+    students_collection = db.collection("studentsDetails")
+
+    for doc_ref in students_collection.stream():
+        student_data = doc_ref.to_dict()
+
+        if student_data:
+            student_details.append(student_data)
+
+    return student_details
+
+# Asynchronous function to mark attendance for a student
+async def markattendanceasync(student_details, stu_id, class_start_time, attendance_cutoff_time):
+
+    student_data = None
+    for student in student_details:
+        if student.get("id") == stu_id:
+            student_data = student
+            break
+
+    if student_data is None:
+        print(f"Student with ID {stu_id} not found.")
+        return
+
+    student_name = student_data.get("name")
     now = datetime.now()
 
-    # Extract the year, month, and date from the current date
     year_folder = now.strftime('%Y')
-    month_folder = now.strftime('%B')  # Use full month name (e.g., "July") instead of the numerical representation
+    month_folder = now.strftime('%B')
     date_folder = now.strftime('%d')
 
-    # Determine the student's attendance status based on the current time
     student_status = get_attendance_status(class_start_time, attendance_cutoff_time)
 
-    # Create a reference to the "Attendance" collection in Firestore
     attendance_ref = db.collection("Attendance")
-
-    # Create a reference to the main folder with the year as the document name
     year_doc_ref = attendance_ref.document(year_folder)
-
-    # Create a reference to the subfolder with the month as the document name
     month_doc_ref = year_doc_ref.collection(month_folder)
-
-    # Create a reference to the student's sub-subfolder with the student_id as the document name
     student_doc_ref = month_doc_ref.document(date_folder)
 
-    # Get the current time
     current_time = now.strftime("%H:%M:%S")
 
-    # Create a reference to the student's folder with the student name as the document name
     student_folder_ref = student_doc_ref.collection("Students").document(stu_id)
 
-    # Update the student's attendance data for the current date and time
     student_data = {
-        'name': stu_id,
+        'name': student_name,
+        'time': current_time,
         'status': student_status,
-        'time': current_time
     }
     student_folder_ref.set(student_data)
 
-
+# Main asynchronous function
 async def main():
-    # Ask the user for the class start time and attendance cutoff time
+    student_details = retrieve_student_details()
     class_start_time, attendance_cutoff_time = get_class_time_input()
 
     cap = cv2.VideoCapture(0)
@@ -101,50 +115,37 @@ async def main():
     imgBackground = cv2.imread('background.png')
 
     while True:
-        # Read frames from the webcam
         success, img = cap.read()
 
-        # Resize the frame for faster face recognition
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-        # Detect face locations and encodings in the current frame
         faceCurFrame = face_recognition.face_locations(imgS)
         encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
         imgBackground[162:162 + 480, 55:55 + 640] = img
-
         imgBackground[44:44 + 633, 800:800 + 414] = imgModeList[0]
 
         if faceCurFrame:
             tasks = []
             for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
-                # Compare face encodings to known encodings
                 matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
                 faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-                print("matches", matches)
-                print("faceDis", faceDis)
 
                 matchIndex = np.argmin(faceDis)
 
-                print("matchIndex", matchIndex)
-                print("face_location ", faceLoc)
-
-                # If an unknown face is detected with the minimum similarity >= 0.4, print "unknown" and skip marking
                 if min(faceDis) >= 0.4:
-                    print("student name: unknown")
+                    print("Student name: unknown")
                     continue
 
                 if matches[matchIndex]:
                     stu_id = studentIds[matchIndex]
-                    print("student name:", studentIds[matchIndex])
-                    # If a known face is detected
+                    print("Student name:", studentIds[matchIndex])
                     y1, x2, y2, x1 = faceLoc
                     y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
                     bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
                     imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
-                    tasks.append(markattendanceasync(stu_id, class_start_time, attendance_cutoff_time))
-
+                    tasks.append(markattendanceasync(student_details, stu_id, class_start_time, attendance_cutoff_time))
                     imgBackground[44:44 + 633, 800:800 + 414] = imgModeList[1]
 
             if tasks:
@@ -152,14 +153,11 @@ async def main():
 
         cv2.imshow("Attendance System", imgBackground)
 
-        # If the 'q' key is pressed, break out of the loop and stop the video capture
         if cv2.waitKey(33) & 0xFF == ord('q'):
             break
 
-    # Release the video capture and close the window
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
--- encoder.py
+++ encoder.py
@@ -6,31 +6,29 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 import tempfile
 
+# Initialize Firebase Admin SDK
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': "attendance-system-0786.appspot.com"
 })
 
+# Initialize Firestore and Storage
 db = firestore.client()
 bucket = storage.bucket()
-
 
 def create_data_dictionary(image_id):
     # Prompt the user to enter details for the image
     stu_name = input(f"Enter Student Name for {image_id}: ")
     data = {
         'id': image_id,
-        'name': stu_name,
-        # Add more fields if needed, for example: 'path': '', 'date_taken': '', etc.
+        'name': stu_name
     }
     return data
-
 
 def upload_data_to_firestore(data, collection_name, document_name):
     doc_ref = db.collection(collection_name).document(document_name)
     doc_ref.set(data)
     print(f"Data uploaded to Firestore for {collection_name}/{document_name}.")
-
 
 def upload_images_to_storage(folder_path):
     path_list = os.listdir(folder_path)
@@ -39,22 +37,21 @@ def upload_images_to_storage(folder_path):
         image_id = os.path.splitext(path)[0]
         file_name = f'{folder_path}/{path}'
 
-        data = create_data_dictionary(image_id)  # Create a data dictionary for the image
+        data = create_data_dictionary(image_id)
 
         blob = bucket.blob(file_name)
-        blob.upload_from_filename(file_name)  # Upload the image without metadata
+        blob.upload_from_filename(file_name)
 
         # Set the metadata for the blob
         blob.metadata = data
         blob.patch()
 
         # Upload the data to Firestore
-        collection_name = "studentsDetails"  # You can change this to the desired collection name
+        collection_name = "studentsDetails"
         document_name = image_id
         upload_data_to_firestore(data, collection_name, document_name)
 
         print("Image and data uploaded:", path)
-
 
 def download_images_from_storage(folder_path):
     path_list = os.listdir(folder_path)
@@ -62,7 +59,7 @@ def download_images_from_storage(folder_path):
     student_ids = []
 
     for path in path_list:
-        file_name = os.path.join(folder_path, path).replace('\\', '/')  # Use os.path.join to handle path separators
+        file_name = os.path.join(folder_path, path).replace('\\', '/')
         _, temp_file_path = tempfile.mkstemp(suffix='.jpg')
         blob = bucket.blob(file_name)
         blob.download_to_filename(temp_file_path)
@@ -72,7 +69,6 @@ def download_images_from_storage(folder_path):
 
     return img_list, student_ids
 
-
 def findencoding(images_list):
     encodeList = []
     for img in images_list:
@@ -81,9 +77,8 @@ def findencoding(images_list):
         encodeList.append(encode)
     return encodeList
 
-
 print('Uploading Images and Data to Firebase Storage and Firestore ...')
-folderPath = 'dataSet'  # Assuming the folder name is "dataSet"
+folderPath = 'dataSet'
 upload_images_to_storage(folderPath)
 
 print('Downloading Images from Firebase Storage ...')
